8c9c92e75d6bf962f16ae694d3d87ccdc023384c
213ze7ea <213ze7ea@lizardeh.onmicrosoft.com>
2025-07-10 20:44:20 +0700

    asd

--- aaaaa.sh
+++ aaaaa.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "OK mulai"
 if [[ "$VERSION" != "$NEW_VERSION" ]]; then
   python3 gofile.py "$LATEST_LINK" "$RC_FOLDER/$RC_FOLDER" "$DD_FOLDER:Archive/FC2/1.Mirror" "$RC_FOLDER/$RC_FOLDER.conf"
   echo "HAS_UPDATE=yes" >> $GITHUB_OUTPUT
